e6f902ad5e3d97b96cb72db7dc98b15107f1c940
Munna Ahmed <munnaahmed2025@gmail.com>
2022-10-11 01:46:00 +0600

    commit

--- src/styles.js
+++ src/styles.js
@@ -4,8 +4,8 @@ const styles = {
     inputRowContainer:
         "bg-orange-50 flex-col flex justify-evenly xs:flex-row px-3",
     inputContainer:
-        "border xs:m-5  mt-5  text-center xs:text-left  bg-orange-400 text-white",
+        "border xs:m-5  mt-5  text-center xs:text-left  bg-orange-400 ",
     input: " solid focus:outline-0 focus:border-black w-full placeholder:text-center pl-2 text-center xs:text-left xs:placeholder:text-left  border-t ",
-    label: "pl-0 xs:pl-2 font-medium",
+    label: "pl-0 xs:pl-2 font-medium text-white",
 };
 export default styles;

e05ed911b040755270702e7d74162ea3bfa0128f
Munna Ahmed <munnaahmed2025@gmail.com>
2022-10-11 22:11:40 +0600

    added mobile responsive

--- src/App.js
+++ src/App.js
@@ -2,33 +2,43 @@ import React, { useState } from "react";
 import styles from "./styles.js";
 
 const App = () => {
-    const [minpx, setminpx] = useState();
-    const [maxpx, setmaxpx] = useState();
-    const [minVw, setMinVw] = useState();
-    const [maxVw, setMaxVw] = useState();
+    const [minPx, setMinPx] = useState(null);
+    const [maxPx, setMaxPx] = useState(null);
+    const [minVw, setMinVw] = useState(null);
+    const [maxVw, setMaxVw] = useState(null);
+    // const [error, setError] = useState("");
     const [responsiveFs, setResponsiveFs] = useState();
+    // console.log(error);
+    const handleminpx = (e) => {
+        // var minPxValue = e.target.value;
+        // console.log(minPxValue);
 
-    const handleminpx = (e) => setminpx(e.target.value);
-    const handlemaxpx = (e) => setmaxpx(e.target.value);
+        setMinPx(e.target.value);
+    };
+    const handlemaxpx = (e) => setMaxPx(e.target.value);
     const handleMinVw = (e) => setMinVw(e.target.value);
     const handleMaxVw = (e) => setMaxVw(e.target.value);
 
-    let minRem = minpx / 16;
-    let maxRem = maxpx / 16;
+    var minRem = minPx / 16;
+    let maxRem = maxPx / 16;
+    console.log(minRem);
+    // prettier-ignore
+    const middleVw = ((100 * (maxPx - minPx)) / (maxVw - minVw)).toPrecision(5);
 
     // prettier-ignore
-    const middleVw = ((100 * (maxpx - minpx)) / (maxVw - minVw)).toPrecision(5);
-
-    // prettier-ignore
-    const middleRem = ((((minVw * maxpx) - (maxVw * minpx)) / (minVw - maxVw)) / 16).toPrecision(5);
+    const middleRem = ((((minVw * maxPx) - (maxVw * minPx)) / (minVw - maxVw)) / 16).toPrecision(5);
 
     const handleSubmit = (e) => {
+        // if (minPx === null) {
+        //     setError("This field cannot be empty");
+        // }
         setResponsiveFs(
             `clamp(${minRem}rem ,${middleVw}vw + ${middleRem}rem ,  ${maxRem}rem)`
         );
         e.preventDefault();
     };
-    console.log(responsiveFs);
+
+    // console.log(responsiveFs);
 
     return (
         <div className="App font-poppins antialiased max-w-4xl mx-auto text-center">
@@ -37,77 +47,121 @@ const App = () => {
                 (Using Clamp)
             </span>
             {/* <h2>How to use</h2> */}
-            <form className={`${styles.form}`}>
+            <form className={`${styles.form}`} onSubmit={handleSubmit}>
                 <div className={`${styles.inputRowContainer}`}>
-                    <div className={`${styles.inputContainer}`}>
-                        <label hmtlfor="min-fs" className={`${styles.label}`}>
-                            Min Font Size
-                        </label>{" "}
-                        <br />
-                        <input
-                            onBlur={handleminpx}
-                            type="number"
-                            id="min-fs"
-                            className={`${styles.input}`}
-                            placeholder="16"
-                            required
-                        ></input>
+                    <div className={`${styles.inputColContainer}`}>
+                        <div className={`${styles.inputContainer}`}>
+                            <div className="bg-orange-400 text-left">
+                                <label
+                                    hmtlfor="min-fs"
+                                    className={`${styles.label}`}
+                                >
+                                    Min Font Size
+                                </label>
+                            </div>
+
+                            <input
+                                onBlur={handleminpx}
+                                type="number"
+                                id="min-fs"
+                                className={`${styles.input}`}
+                                placeholder="16"
+                                required
+                            ></input>
+                            <p className={`${styles.error}`}>
+                                This field cannot be empty
+                            </p>
+                        </div>
                     </div>
-                    <div className={`${styles.inputContainer}`}>
-                        <label hmtlfor="max-fs" className={`${styles.label}`}>
-                            Max Font Size
-                        </label>{" "}
-                        <br />
-                        <input
-                            onBlur={handlemaxpx}
-                            type="number"
-                            id="max-fs"
-                            className={`${styles.input}`}
-                            placeholder="20"
-                            required
-                        ></input>
+                    <div className={`${styles.inputColContainer}`}>
+                        <div className={`${styles.inputContainer}`}>
+                            <div className="bg-orange-400 text-left">
+                                <label
+                                    hmtlfor="max-fs"
+                                    className={`${styles.label}`}
+                                >
+                                    Max Font Size
+                                </label>
+                            </div>
+
+                            <input
+                                onBlur={handlemaxpx}
+                                type="number"
+                                id="max-fs"
+                                className={`${styles.input}`}
+                                placeholder="20"
+                                required
+                            ></input>
+                            <p className={`${styles.error}`}>
+                                This field cannot be empty
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className={`${styles.inputRowContainer}`}>
-                    <div className={`${styles.inputContainer}`}>
-                        <label hmtlfor="min-vw" className={`${styles.label}`}>
-                            Min Viewport Size
-                        </label>{" "}
-                        <br />
-                        <input
-                            onBlur={handleMinVw}
-                            type="number"
-                            id="min-vw"
-                            className={`${styles.input}`}
-                            placeholder="320"
-                            Required
-                        ></input>
+                    <div className={`${styles.inputColContainer}`}>
+                        <div className={`${styles.inputContainer}`}>
+                            <div className="bg-orange-400 text-left">
+                                <label
+                                    hmtlfor="min-vw"
+                                    className={`${styles.label}`}
+                                >
+                                    Min Viewport Size
+                                </label>
+                            </div>
+
+                            <input
+                                onBlur={handleMinVw}
+                                type="number"
+                                id="min-vw"
+                                className={`${styles.input} `}
+                                placeholder="320"
+                                required
+                            ></input>
+                            <p className={`${styles.error}`}>
+                                This field cannot be empty
+                            </p>
+                        </div>
                     </div>
-                    <div className={`${styles.inputContainer} mb-5`}>
-                        <label hmtlfor="max-vw" className={`${styles.label}`}>
-                            Max Viweport Size
-                        </label>{" "}
-                        <br />
-                        <input
-                            onBlur={handleMaxVw}
-                            type="number"
-                            id="max-vw"
-                            className={`${styles.input} `}
-                            placeholder="1200"
-                            required
-                        ></input>
+
+                    <div className={`${styles.inputColContainer} `}>
+                        <div className={`${styles.inputContainer} mb-10`}>
+                            <div className="bg-orange-400 text-left">
+                                <label
+                                    hmtlfor="max-vw"
+                                    className={`${styles.label}`}
+                                >
+                                    Max Viweport Size
+                                </label>
+                            </div>
+
+                            <input
+                                onBlur={handleMaxVw}
+                                type="number"
+                                id="max-vw"
+                                className={`${styles.input} `}
+                                placeholder="1200"
+                                required
+                            ></input>
+                            <p className={`${styles.error}`}>
+                                This field cannot be empty
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <button
-                    onClick={handleSubmit}
-                    type="submit"
-                    className="py-2 font-bold  border-black hover:bg-orange-300 hover:text-orange-700 text-orange-600 bg-orange-200 mt-5 transition duration-300"
-                >
-                    Submit
-                </button>
+                <div>
+                    <button
+                        type="submit"
+                        className="py-2 font-bold  border-black hover:bg-orange-300 hover:text-orange-700 text-orange-600 bg-orange-200  transition duration-300 w-40 rounded mt-10"
+                    >
+                        Submit
+                    </button>
+                </div>
             </form>
             <div>
-                <p className="text-center mt-5">{responsiveFs}</p>
+                <p className="text-center mt-5 font-semibold text-orange-500 ">
+                    {responsiveFs}
+                </p>
             </div>
         </div>
     );
--- src/styles.js
+++ src/styles.js
@@ -1,11 +1,12 @@
 const styles = {
     heading1: "xs:text-4xl text-3xl font-black text-center pt-8",
-    form: " text  flex flex-col mx-auto xxs:w-full px-2",
+    form: "  rounded-lg  text  flex flex-col mx-auto xxs:w-full px-2  ",
     inputRowContainer:
-        "bg-orange-50 flex-col flex justify-evenly xs:flex-row px-3",
-    inputContainer:
-        "border xs:m-5  mt-5  text-center xs:text-left  bg-orange-400 ",
-    input: " solid focus:outline-0 focus:border-black w-full placeholder:text-center pl-2 text-center xs:text-left xs:placeholder:text-left  border-t ",
-    label: "pl-0 xs:pl-2 font-medium text-white",
+        "bg-orange-100 flex-col flex justify-evenly xs:flex-row ",
+    inputColContainer: "   mt-10  text-center xs:text-left ",
+    inputContainer: "mx-2 text-center xs:text-left",
+    input: "peer solid focus:outline-0 focus:border-orange-500 w-full placeholder:text-center placeholder:text-orange-200 pl-2 text-center xs:text-left xs:placeholder:text-left  border  text-orange-500 font-semibold ",
+    label: "pl-2 font-medium text-white xs:text-left text-center w-full block",
+    error: "hidden peer-invalid:block text-red-600 font-medium bg-orange-0",
 };
 export default styles;
